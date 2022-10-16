3e4efd965a3464d97a55e5ef96a5ef6e5aa6c0bd
DevelopJKong <ybaram40@gmail.com>
2022-10-16 11:36:19 +0900

    #15.15 Using the Token 완료

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,13 +1,27 @@
 import { LOCALSTORAGE_TOKEN } from './constants';
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:5000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'x-jwt': authTokenVar() || '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,10 +1,41 @@
+import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import { isLoggedInVar } from '../apollo';
+import { meQuery } from '../__generated__/meQuery';
+
+const Loading = styled.div`
+  ${tw`h-screen flex justify-center items-center`}
+`;
+
+const LoadingText = styled.span`
+  ${tw`font-medium text-xl tracking-wide`}
+`;
+
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
 
 const Login = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  console.log(data, error);
+  if (!data || loading || error) {
+    return (
+      <Loading>
+        <LoadingText>Loading...</LoadingText>
+      </Loading>
+    );
+  }
   return (
     <div>
-      <h1>Logged In</h1>
-      <button onClick={() => isLoggedInVar(false)}>Log out</button>
+      <h1>{data.me.role}</h1>
     </div>
   );
 };
