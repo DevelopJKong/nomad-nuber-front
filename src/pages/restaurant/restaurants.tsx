ba5daf26946ece9a05bd8db079b21693c0450aa7
DevelopJKong <ybaram40@gmail.com>
2022-10-25 07:45:51 +0900

    #19.2 Restaurants List 완료

--- src/pages/restaurant/restaurants.tsx
+++ src/pages/restaurant/restaurants.tsx
@@ -1,9 +1,40 @@
 import { gql, useQuery } from "@apollo/client";
-import React from "react";
+import styled from "styled-components";
 import {
   restaurantsPageQuery,
   restaurantsPageQueryVariables,
 } from "../../__generated__/restaurantsPageQuery";
+import tw from "twin.macro";
+
+const Container = styled.div``;
+
+const Form = styled.div`
+  ${tw`bg-gray-800 w-full py-40 flex items-center justify-center`}
+`;
+
+const Input = styled.input`
+  ${tw`rounded-md border-0 w-3/12`}
+`;
+
+const CategoryWrapper = styled.div`
+  ${tw`max-w-screen-2xl mx-auto mt-8`}
+`;
+
+const CategoryContainer = styled.div`
+  ${tw`flex justify-around max-w-xs mx-auto`}
+`;
+
+const Categories = styled.div`
+  ${tw`flex flex-col items-center`}
+`;
+
+const Category = styled.div`
+  ${tw`w-14 h-14 hover:bg-gray-100 rounded-full bg-red-500 bg-cover cursor-pointer`}
+`;
+
+const Text = styled.span`
+  ${tw`mt-1 text-sm text-center font-medium`}
+`;
 
 const RESTAURANTS_QUERY = gql`
   query restaurantsPageQuery($input: RestaurantsInput!) {
@@ -49,7 +80,27 @@ const Restaurants = () => {
     },
   );
 
-  return <div>restaurant</div>;
+  return (
+    <Container>
+      <Form>
+        <Input type='Search' placeholder='Search restaurant...' />
+      </Form>
+      {!loading && (
+        <CategoryWrapper>
+          <CategoryContainer>
+            {data?.allCategories.categories?.map((category, index) => (
+              <Categories key={index}>
+                <Category style={{ backgroundImage: `url(${category.coverImage})` }}>
+                  {category.name}
+                </Category>
+                <Text>{category.name}</Text>
+              </Categories>
+            ))}
+          </CategoryContainer>
+        </CategoryWrapper>
+      )}
+    </Container>
+  );
 };
 
 export default Restaurants;
