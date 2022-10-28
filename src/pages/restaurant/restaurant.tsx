fc3ef4496373c180d8b5916896ac87d0459f539b
DevelopJKong <ybaram40@gmail.com>
2022-10-28 23:52:48 +0900

    #19.9 Restaurant part Two 완료

--- src/pages/restaurant/restaurant.tsx
+++ src/pages/restaurant/restaurant.tsx
@@ -1,8 +1,32 @@
-import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
 import { restaurant, restaurantVariables } from "../../__generated__/restaurant";
+import tw from "twin.macro";
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  ${tw`bg-red-500 bg-center bg-cover py-48`}
+`;
+
+const Content = styled.div`
+  ${tw`bg-white w-3/12 py-4 pl-48`}
+`;
+
+const Title = styled.h4`
+  ${tw`text-4xl mb-3`}
+`;
+
+const CategoryName = styled.h5`
+  ${tw`text-sm font-light`}
+`;
+
+const Address = styled.h6`
+  ${tw`text-sm font-light`}
+`;
 
 const RESTAURANT_QUERY = gql`
   query restaurant($input: RestaurantInput!) {
@@ -18,7 +42,7 @@ const RESTAURANT_QUERY = gql`
 `;
 
 interface IRestaurantParams {
-  id: string; 
+  id: string;
 }
 
 const Restaurant = () => {
@@ -30,7 +54,15 @@ const Restaurant = () => {
       },
     },
   });
-  return <h1>Restaurant</h1>;
+  return (
+    <Container>
+      <Header style={{ backgroundColor: `url(${data?.restaurant.restaurant?.coverImg})` }}></Header>
+      <Content>
+        <Title>{data?.restaurant.restaurant?.name}</Title>
+        <CategoryName>{data?.restaurant.restaurant?.category?.name}</CategoryName>
+      </Content>
+    </Container>
+  );
 };
 
 export default Restaurant;
