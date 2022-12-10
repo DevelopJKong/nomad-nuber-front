69347528f69329ff5fa21346c4fc8d70bf199dff
cafe small house <ybaram40@gmail.com>
2022-12-10 23:20:47 +0900

    #25.7 Driver Dashboard part Three 완료

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
+interface ICoords {
+   lat: number;
+   lng: number;
+}
+
 const Container = styled.div``;
 
 const Content = styled.div`
-   ${tw`py-20 bg-gray-800 overflow-hidden`}
+   ${tw`overflow-hidden`}
    width:${window.innerWidth}px;
-   height: 95vh;
+   height: 50vh;
 `;
+
+const MapContent = styled.div<{ lat: any; lng: any }>`
+   ${tw`h-10 w-10 bg-white rounded-full flex justify-center items-center text-lg`}
+`;
+
 const Dashboard = () => {
+   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+   const [map, setMap] = useState<any>();
+   const [maps, setMaps] = useState<any>();
+   const onSuccess = ({ coords: { latitude, longitude } }: any) => {
+      setDriverCoords({ lat: latitude, lng: longitude });
+   };
+   const onError = (error: any) => {
+      console.log(error);
+   };
+   useEffect(() => {
+      navigator.geolocation.watchPosition(onSuccess, onError, {
+         enableHighAccuracy: true,
+      });
+   }, []);
+
+   useEffect(() => {
+      if (map && maps) {
+         map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+      }
+   }, [driverCoords.lat, driverCoords.lng]);
+
+   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+      setMap(map);
+      setMaps(maps);
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+   };
    const GOOGLE_MAP_KEY = process.env.REACT_APP_GOOGLE_MAP_KEY;
    return (
       <Container>
          <Content>
             <GoogleMapReact
-               defaultZoom={20}
+               yesIWantToUseGoogleMapApiInternals
+               onGoogleApiLoaded={onApiLoaded}
+               defaultZoom={16}
                defaultCenter={{ lat: 59.95, lng: 30.33 }}
                bootstrapURLKeys={{ key: GOOGLE_MAP_KEY ? GOOGLE_MAP_KEY : "" }}
-            ></GoogleMapReact>
+            >
+               <MapContent lat={driverCoords.lat} lng={driverCoords.lng}>
+                  💕
+               </MapContent>
+            </GoogleMapReact>
          </Content>
       </Container>
    );
